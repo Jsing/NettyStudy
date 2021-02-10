2b23ec152e6889341c76ddf31c4f03d2cfa01b1d
Jsing <jsing@contec.kr>
2021-02-10 17:37:22 +0900

    Starter

---
+++ src/main/java/netty/netty/study/server/NettyStudyServerApplication.java
@@ -0,0 +1,13 @@
+package netty.netty.study.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyStudyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyStudyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/netty/netty/study/server/NettyStudyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package netty.netty.study.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyStudyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
